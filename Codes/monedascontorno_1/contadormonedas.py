ffa58ce39b180e1ebefd1956df134ee3d149d307
Felipe <u202110902@upc.edu.pe>
2023-01-25 10:20:56 -0500

    arreglo

---
+++ Codes/monedascontorno_1/contadormonedas.py
@@ -0,0 +1,24 @@
+from cv2 import cv2
+import numpy as np
+
+valorGauss=1
+valorKernel=7
+original=cv2.imread('monedassoles.jpg')
+gris=cv2.cvtColor(original,cv2.COLOR_BGR2GRAY)
+gauss=cv2.GaussianBlur(gris, (valorGauss,valorGauss), 0)
+canny=cv2.Canny(gauss, 60,100)
+kernel=np.ones((valorKernel,valorKernel),np.uint8)
+cierre=cv2.morphologyEx(canny, cv2.MORPH_CLOSE, kernel)
+
+contornos, jerarquía=cv2.findContours(cierre.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+
+print("monedas encontradas: {}".format(len(contornos)))
+cv2.drawContours(original, contornos, -1, (0,0,255),2)
+#Mostrar resultados
+#cv2.imshow("Grises",gris)
+#cv2.imshow("gauss",gauss)
+#cv2.imshow("canny",canny)
+#cv2.imshow("cierre",cierre)
+
+cv2.imshow("Resultado", original)
+cv2.waitKey(0)
---
+++ Codes/monedascontorno_2/contorno.py
@@ -0,0 +1,13 @@
+from cv2 import cv2
+imagen=cv2.imread('contorno.jpg')
+grises=cv2.cvtColor(imagen,cv2.COLOR_BGR2GRAY)
+_,umbral=cv2.threshold(grises,100,255,cv2.THRESH_BINARY)
+contorno,jerarquía = cv2.findContours(umbral,cv2.RETR_LIST,cv2.CHAIN_APPROX_SIMPLE)
+cv2.drawContours(imagen,contorno,-1,(251, 63, 52),3)
+
+#Mostrar
+cv2.imshow('Imagen original',imagen)
+cv2.imshow('Imagen en grises',grises)
+cv2.imshow('Imagen Umbral',umbral)
+cv2.waitkey(0)
+cv2.destroyAllWindows()
