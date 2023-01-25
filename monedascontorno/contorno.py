a8ef624b1f21cc73dfee97300337735f54f96852
Felipe <u202110902@upc.edu.pe>
2023-01-24 21:00:47 -0500

    avance

--- monedascontorno/contorno.py
+++ monedascontorno/contorno.py
@@ -1,5 +1,14 @@
-from cv2 import cv2
+import cv2
 imagen=cv2.imread('contorno.jpg')
-cv2.imshow('imagen',imagen)
+grises=cv2.cvtColor(imagen,cv2.COLOR_BGR2GRAY)
+_,umbral=cv2.threshold(grises,100,255,cv2.THRESH_BINARY)
+contorno,jerarquía = cv2.findContours(umbral,cv2.RETR_LIST,cv2.CHAIN_APPROX_SIMPLE)
+cv2.drawContours(imagen,contorno,-1,(251, 63, 52),3)
+
+
+#Mostrar
+cv2.imshow('Imagen original',imagen)
+#cv2.imshow('Imagen en grises',grises)
+#cv2.imshow('Imagen Umbral',umbral)
 cv2.waitKey(0)
 cv2.destroyAllWindows()
