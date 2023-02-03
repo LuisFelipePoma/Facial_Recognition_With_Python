458d10bd515d99c8a0ccf304030faf728d581fd6
Felipe <u202110902@upc.edu.pe>
2023-02-03 12:16:50 -0500

    capturas del rostro EN CARPETA

--- Codes/reconocimientofacial1/capaentrada_1.py
+++ Codes/reconocimientofacial1/capaentrada_1.py
@@ -1,19 +1,41 @@
 import cv2 as cv
 import os
 
+import imutils
+
+modelo = 'FotosYo'
+ruta1 = 'C:/Users/pms_l/Documents/Cursos/Reconocimiento Facial - Python/Codes/reconocimientofacial1'
+rutaCompleta = ruta1 + '/' + modelo
+if not os.path.exists(rutaCompleta):
+    os.mkdir(rutaCompleta)
 
 camara = cv.VideoCapture(0)
 ruidos = cv.CascadeClassifier(
     'C:\\Users\pms_l\Documents\Cursos\Reconocimiento Facial - Python\Material\opencv\data\haarcascades\haarcascade_frontalface_default.xml')
 
+id = 0
+
 while True:
-    _, captura = camara.read()
+    respuesta, captura = camara.read()
+    if not respuesta: break
+    captura = imutils.resize(captura, width=640)
     grises = cv.cvtColor(captura, cv.COLOR_BGR2GRAY)
+    idCaptura = captura.copy()
+
     cara = ruidos.detectMultiScale(grises, 1.3, 4)
     for (x, y, e1, e2) in cara:
         cv.rectangle(captura, (x, y), (x + e1, y + e2), (0, 255, 0), 2)
+        rostroCapturado = idCaptura[y:y + e2, x:x + e1]
+        rostroCapturado = cv.resize(rostroCapturado, (160, 160), interpolation=cv.INTER_CUBIC)
+        cv.imwrite(rutaCompleta + '/rostro_{}.png'.format(id), rostroCapturado)
+        id = id + 1
+
     cv.imshow("Resultado rostro", captura)
-    if cv.waitKey(1) == ord("s"):
+
+    # if cv.waitKey(1) == ord("s"):
+    #     break
+
+    if id == 350:
         break
 camara.release()
 cv.destroyAllWindows()
